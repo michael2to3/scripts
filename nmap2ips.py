4944028189f0129521028c299095eff7bb764816
Mikhail Gaile <michael2ga3@gmail.com>
2024-03-03 18:49:50 +0300

    feat: if hostname is empty use address in nmap2ips

--- nmap2ips.py
+++ nmap2ips.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from typing import NoReturn
 import argparse
 import sys
@@ -7,17 +8,27 @@ import xml.etree.ElementTree as ET
 def parse_nmap_xml(xml_input: str) -> NoReturn:
     root = ET.fromstring(xml_input)
     for host in root.findall(".//host"):
+        domain = None
+
         hostnames = host.find(".//hostnames")
         if hostnames is not None:
-            for hostname in hostnames.findall(".//hostname"):
-                domain = hostname.get("name")
-                ports = host.find(".//ports")
-                if ports is not None:
-                    for port in ports.findall(".//port"):
-                        state = port.find(".//state")
-                        if state is not None and state.get("state") == "open":
-                            portid = port.get("portid")
-                            print(f"{domain}:{portid}")
+            hostname_element = hostnames.find(".//hostname")
+            if hostname_element is not None:
+                domain = hostname_element.get("name")
+
+        if domain is None:
+            address_element = host.find(".//address")
+            if address_element is not None:
+                domain = address_element.get("addr")
+
+        if domain:
+            ports = host.find(".//ports")
+            if ports is not None:
+                for port in ports.findall(".//port"):
+                    state = port.find(".//state")
+                    if state is not None and state.get("state") == "open":
+                        portid = port.get("portid")
+                        print(f"{domain}:{portid}")
 
 
 def main() -> NoReturn:
