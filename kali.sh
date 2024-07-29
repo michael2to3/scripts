8cc2a8e8abbb5f4ed25785c817cd49208a117475
Mikhail Gaile <michael2ga3@gmail.com>
2024-07-29 12:47:50 +0300

    fix: kali has auto save last change

--- kali.sh
+++ kali.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VOLUME=""
+CONTAINER_NAME="mkali"
 
 if [ $# -gt 0 ]; then
     for ARG in "$@"
@@ -12,8 +13,13 @@ fi
 #apt update && apt -y install kali-linux-headless
 #apt update && apt -y install kali-linux-large
 docker run \
+    --name "$CONTAINER_NAME" \
     --mount src=kali-root,dst=/root \
     --mount src=kali-postgres,dst=/var/lib/postgresql \
     -ti \
     $VOLUME \
-    kalilinux/kali-rolling
+    kalilinux/kali-rolling:latest
+
+docker commit "$CONTAINER_NAME" kalilinux/kali-rolling:latest
+
+docker rm "$CONTAINER_NAME"
