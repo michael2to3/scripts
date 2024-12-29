de34c092f4b455d5218f21364b3cb4edad028ee2
Mikhail Gaile <michael2ga3@gmail.com>
2024-12-29 23:51:30 +0300

    fix: align apk file in new android version

--- signAndroidApp.sh
+++ signAndroidApp.sh
@@ -5,9 +5,10 @@ KEY_ALIAS='key0'
 APK_FILE="$1"
 OUTPUT="$2"
 
-zipalign -v 4 $APK_FILE "$OUTPUT/app-release-aligned.apk"
+zipalign -p -f -v 4 "$APK_FILE" "$OUTPUT/app-release-aligned-stage1.apk"
+zipalign -p -f -v 4 "$OUTPUT/app-release-aligned-stage1.apk" "$OUTPUT/app-release-aligned.apk"
 apksigner sign --ks "$KEY_PATH" --ks-key-alias "$KEY_ALIAS" --out "$OUTPUT/app-release-signed.apk" "$OUTPUT/app-release-aligned.apk"
-if ( apksigner verify "$OUTPUT/app-release-signed.apk" ); then
+if (apksigner verify "$OUTPUT/app-release-signed.apk"); then
   echo "Successfully signed $OUTPUT/app-release-signed.apk"
 else
   echo "Failed to sign $OUTPUT/app-release-signed.apk"
