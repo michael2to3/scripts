9de572a603e4229d27eef064606c4c14fe6660e1
Mikhail Gaile <michael2ga3@gmail.com>
2025-05-11 13:46:20 +0300

    feat: less shit

--- adblock/telegram_webk_disable_sipe.js
+++ adblock/telegram_webk_disable_sipe.js
@@ -7,17 +7,37 @@
 // @grant        none
 // ==/UserScript==
 
+
 (function () {
   'use strict';
+
+  const THRESHOLD = 10;
+  let startX = 0;
+  let startY = 0;
+
+  const onTouchStart = (e) => {
+    if (e.touches.length !== 1) return;
+    const t = e.touches[0];
+    startX = t.clientX;
+    startY = t.clientY;
+  };
 
   const onTouchMove = (e) => {
     if (e.touches.length !== 1) return;
 
+    const t = e.touches[0];
+    const dx = t.clientX - startX;
+    const dy = t.clientY - startY;
+
+    if (Math.abs(dx) > Math.abs(dy) + THRESHOLD) {
+      if (e.target.closest('input, textarea, [contenteditable="true"]')) return;
+
       e.preventDefault();
       e.stopImmediatePropagation();
+    }
   };
 
   const opts = { capture: true, passive: false };
+  window.addEventListener('touchstart', onTouchStart, opts);
   window.addEventListener('touchmove', onTouchMove, opts);
 })();
-
